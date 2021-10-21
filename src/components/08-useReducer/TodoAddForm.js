94490ba4c2b011bcb3f89e0eebd90b436976cf73
cristian ortiz navia <cristian.ortiznavia@gmail.com>
2021-10-20 21:45:17 -0300

    refactor of TodoApp in multiples components

---
+++ src/components/08-useReducer/TodoAddForm.js
@@ -0,0 +1,51 @@
+import React from "react";
+import useForm from "../hooks/useForm";
+
+const TodoAddForm = ({ handleAddTodo }) => {
+  //using useForm custom hook to state the form input value, destructuring the only value of this form
+  //from original formValues state object in useForm
+  const [{ description }, handleInputChange, reset] = useForm({
+    description: "",
+  });
+  //function to process form submit event
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //if input is empty
+    if (description.trim().length <= 1) {
+      return;
+    }
+    //create a new todo
+    const newTodo = {
+      id: new Date().getTime(),
+      desc: description,
+      done: false,
+    };
+    //add the new to using function referenced from TodoApp />
+    handleAddTodo(newTodo);
+    //reset the form usgina function from useForm hook
+    reset();
+  };
+
+  return (
+    <>
+      <h3>Add TODO</h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="form-control"
+          type="text"
+          name="description"
+          placeholder="aprender..."
+          value={description}
+          onChange={handleInputChange}
+        />
+        <div className="d-grid gap-2">
+          <button className="btn btn-outline-primary mt-2" type="submit">
+            Add
+          </button>
+        </div>
+      </form>
+    </>
+  );
+};
+
+export default TodoAddForm;
--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,31 +1,19 @@
 import React, { useEffect, useReducer } from "react";
 import { todoReducer } from "./todoReducer";
-import useForm from "../hooks/useForm";
+
 import "./styleTodo.css";
 import TodoList from "./TodoList";
+import TodoAddForm from "./TodoAddForm";
 
 //this  function defines the initialState of the reducer
 const init = () => {
-  //get the todos items, and if is empty return an empty array
+  //get the todos items, and if LocalStorage is is empty return an empty array
   return JSON.parse(localStorage.getItem("todos")) || [];
-  // return [
-  //   {
-  //     id: new Date().getTime(),
-  //     desc: "Aprender React Hooks",
-  //     done: false,
-  //   },
-  // ];
 };
 
 const TodoApp = () => {
   //the reducer keep the reference of TODO state and render again when action modify this state
   const [todos, dispatch] = useReducer(todoReducer, [], init);
-
-  //using useForm custom hook to state the form input value, destructuring the only value of this form
-  //from original formValues state object in useForm
-  const [{ description }, handleInputChange, reset] = useForm({
-    description: "",
-  });
 
   //effect to write in localStorage when the TODO list is updated
   useEffect(() => {
@@ -48,28 +36,9 @@ const TodoApp = () => {
       payload: id,
     });
   };
-  //function to process form submit event
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    //if input is empty
-    if (description.trim().length <= 1) {
-      return;
-    }
-    //create a new todo
-    const newTodo = {
-      id: new Date().getTime(),
-      desc: description,
-      done: false,
-    };
-    //define the action
-    const action = {
-      type: "add",
-      payload: newTodo,
-    };
+  const handleAddTodo = (newTodo) => {
     //call de dispatch to send action to reducer
-    dispatch(action);
-    //reset the form usgina function from useForm hook
-    reset();
+    dispatch({ type: "add", payload: newTodo });
   };
 
   return (
@@ -85,23 +54,7 @@ const TodoApp = () => {
           />
         </div>
         <div className="col-5">
-          <h3>Add TODO</h3>
-
-          <form onSubmit={handleSubmit}>
-            <input
-              className="form-control"
-              type="text"
-              name="description"
-              placeholder="aprender..."
-              value={description}
-              onChange={handleInputChange}
-            />
-            <div className="d-grid gap-2">
-              <button className="btn btn-outline-primary mt-2" type="submit">
-                Add
-              </button>
-            </div>
-          </form>
+          <TodoAddForm handleAddTodo={handleAddTodo} />
         </div>
       </div>
     </div>
